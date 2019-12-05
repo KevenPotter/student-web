8dd81500aceb59397278c5226ed8502c9aa6e4ed
KevenPotter <keven_potter@163.com>
2019-12-05 17:21:39 +0800

    Commit

--- assets/scripts/students.js
+++ assets/scripts/students.js
@@ -1,4 +1,5 @@
 var studentsTableTBody = $('#studentsTableTBody');
+var studentsPage = $('#studentsPage');
 $(document).ready(function () {
     loadStudentsList();
 });
@@ -10,9 +11,9 @@ function loadStudentsList() {
         success: function (data) {
             console.log(data);
             var studentsArray = data.data.list;
-            for (var i = 0, length = studentsArray.length; i < length; i++) {
-                console.log(studentsArray[i]);
-                var item = studentsArray[i];
+            for (var studentIndex = 0, length = studentsArray.length; studentIndex < length; studentIndex++) {
+                console.log(studentsArray[studentIndex]);
+                var item = studentsArray[studentIndex];
                 studentsTableTBody.append('<tr> ' +
                     '<td>' + item.id + '</td>' +
                     '<td>' + item.studentId + '</td>' +
@@ -23,7 +24,15 @@ function loadStudentsList() {
                     '<td>' + item.clazz + '</td>' +
                     '<td>' + item.address + '</td>' +
                     '</tr>');
+
             }
+            var pages = data.data.pages;
+            console.log(data.data.pages);
+            studentsPage.append('<li><a href="#">&laquo;</a></li>');
+            for (var pageIndex = 1; pageIndex <= pages; pageIndex++) {
+                studentsPage.append('<li><a href="#">' + pageIndex + '</a></li>');
+            }
+            studentsPage.append('<li><a href="#">&raquo;</a></li>');
         }
     });
 }
