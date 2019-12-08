e7cb4c90f8f804ec51493fbf82185ee61a8c40cb
KevenPotter <keven_potter@163.com>
2019-12-08 14:43:06 +0800

    Commit

--- assets/scripts/students.js
+++ assets/scripts/students.js
@@ -2,9 +2,12 @@ var studentsTableTBody = $('#studentsTableTBody');
 var studentsPage = $('#studentsPage');
 var studentId = null;
 var studentName = null;
+var departmentsSelect = $('#departmentsSelect');
 var pageIndex = 1;
+
 $(document).ready(function () {
     loadStudentsList(studentId, studentName, pageIndex);
+    a();
 });
 
 function loadStudentsList(studentId, studentName, pageIndex) {
@@ -12,11 +15,11 @@ function loadStudentsList(studentId, studentName, pageIndex) {
     clearHtml(studentsPage);
     var requestParam = {"studentId": studentId, "name": studentName, "pageNo": pageIndex, "pageSize": 5};
     $.ajax({
-        url: studentManagementSystem + "/student/student",
+        url: studentManagementSystem + "/student/students",
         type: "GET",
+        dataType: "json",
         data: requestParam,
         success: function (data) {
-            console.log(data);
             if (data.code == "100002") return;
             var studentsArray = data.data.list;
             for (var studentIndex = 0, length = studentsArray.length; studentIndex < length; studentIndex++) {
@@ -26,12 +29,21 @@ function loadStudentsList(studentId, studentName, pageIndex) {
                     '<td>' + item.studentId + '</td>' +
                     '<td>' + item.name + '</td>' +
                     '<td>' + item.sex + '</td>' +
-                    '<td>' + item.departmentId + '</td>' +
+                    '<td id="departmentId_' + studentIndex + '">' + item.departmentId + '</td>' +
                     '<td>' + item.majorId + '</td>' +
                     '<td>' + item.grade + '</td>' +
                     '<td>' + item.clazz + '</td>' +
                     '<td>' + item.address + '</td>' +
                     '</tr>');
+                $.ajax({
+                    url: studentManagementSystem + "/department/department/" + item.departmentId,
+                    type: "GET",
+                    dataType: "json",
+                    async: false,
+                    success: function (data) {
+                        $('#departmentId_' + studentIndex + '').html(data.data.departmentName);
+                    }
+                });
             }
             var pageNum = data.data.pageNum;
             var pages = data.data.pages;
@@ -45,6 +57,24 @@ function loadStudentsList(studentId, studentName, pageIndex) {
                     loadStudentsList(studentId, studentName, page);
                 }
             });
+        }
+    });
+}
+
+function a() {
+    $.ajax({
+        url: studentManagementSystem + "/department/departments",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            var departmentsArray = data.data;
+            departmentsSelect.append('<option value="' + 0 + '">请选择系别</option>');
+            for (var departmentIndex = 0, length = departmentsArray.length; departmentIndex < length; departmentIndex++) {
+                var item = departmentsArray[departmentIndex];
+                departmentsSelect.append('<option id="' + departmentIndex + '" value="' + item.departmentId + '">' + item.departmentName + '</option>');
+                departmentsSelect.selectpicker("refresh");
+                departmentsSelect.selectpicker("render");
+            }
         }
     });
 }
