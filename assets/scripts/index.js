a9b11f24c36cc3d83186b164a071a577346e19d4
KevenPotter <keven_potter@163.com>
2019-12-27 17:21:58 +0800

    Commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -126,26 +126,25 @@ function getDashboardData() {
  * @description 对后端服务器发送请求以统计在线人数
  */
 function onlineUserCounts() {
-    var socket;
-    socket = new WebSocket(studentManagementSystemWebSocket + "/dashboard");
+    var onlineUserCountSocket = new WebSocket(studentManagementSystemWebSocket + "/dashboard");
     //打开事件
-    socket.onopen = function () {
+    onlineUserCountSocket.onopen = function () {
         console.log("Socket 已打开");
         //socket.send("这是来自客户端的消息" + location.href + new Date());
     };
     //获得消息事件
-    socket.onmessage = function (msg) {
+    onlineUserCountSocket.onmessage = function (msg) {
         console.log(msg.data);
     };
     //关闭事件
-    socket.onclose = function () {
+    onlineUserCountSocket.onclose = function () {
         console.log("Socket已关闭");
     };
     //发生了错误事件
-    socket.onerror = function () {
+    onlineUserCountSocket.onerror = function () {
         alert("Socket发生了错误");
     };
     $(window).on("unload", function (e) {
-        socket.close();
+        onlineUserCountSocket.close();
     });
 }
