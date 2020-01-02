f4beb34f270b19135b4e9828090ca7c76eabc8d5
KevenPotter <keven_potter@163.com>
2020-01-02 14:47:17 +0800

    Commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,74 +13,85 @@ $(document).ready(function () {
  * @description 展示各系男女人数
  */
 function loadSexBar() {
-    var myChart = echarts.init($('#sexBar')[0], 'macarons');
-    var option = null;
-    var xAxisData = [];
-    var data1 = [];
-    var data2 = [];
-    for (var i = 0; i < 100; i++) {
-        xAxisData.push('类目' + i);
-        data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
-        data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
-    }
-    option = {
-        title: {
-            text: '男女人数'
-        },
-        legend: {
-            data: ['男', '女'],
-            align: 'left'
-        },
-        toolbox: {
-            show: true,
-            feature: {
-                magicType: {
-                    type: ['stack', 'tiled']
-                },
-                restore: {
-                    show: true
-                },
-                dataZoom: {
-                    show: true
-                },
-                saveAsImage: {
-                    pixelRatio: 2
+    $.ajax({
+        url: studentManagementSystem + "/index/sexStatistics",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            if (data.code == SUCCESS_MARK) {
+                console.log(data.data);
+                console.log(data.data.length);
+                var myChart = echarts.init($('#sexBar')[0], 'macarons');
+                var option;
+                var xAxisData = [];
+                var numberOfMales = [];
+                var numberOfFemales = [];
+                for (var i = 0; i < data.data.length; i++) {
+                    xAxisData.push(data.data[i].departmentName);
+                    numberOfMales.push(data.data[i].numberOfMales);
+                    numberOfFemales.push(data.data[i].numberOfFemales);
                 }
+                option = {
+                    title: {
+                        text: '学生男女人数'
+                    },
+                    legend: {
+                        data: ['男', '女'],
+                        align: 'left'
+                    },
+                    toolbox: {
+                        show: true,
+                        feature: {
+                            magicType: {
+                                type: ['stack', 'tiled']
+                            },
+                            restore: {
+                                show: true
+                            },
+                            dataZoom: {
+                                show: true
+                            },
+                            saveAsImage: {
+                                pixelRatio: 2
+                            }
+                        }
+                    },
+                    tooltip: {},
+                    xAxis: {
+                        data: xAxisData,
+                        silent: false,
+                        splitLine: {
+                            show: false
+                        }
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '男',
+                        type: 'bar',
+                        data: numberOfMales,
+                        animationDelay: function (idx) {
+                            return idx * 10;
+                        }
+                    }, {
+                        name: '女',
+                        type: 'bar',
+                        data: numberOfFemales,
+                        animationDelay: function (idx) {
+                            return idx * 10 + 100;
+                        }
+                    }],
+                    animationEasing: 'elasticOut',
+                    animationDelayUpdate: function (idx) {
+                        return idx * 5;
+                    }
+                };
+                if (option && typeof option === "object") {
+                    myChart.setOption(option, true);
+                }
+                resize(myChart);
             }
-        },
-        tooltip: {},
-        xAxis: {
-            data: xAxisData,
-            silent: false,
-            splitLine: {
-                show: false
-            }
-        },
-        yAxis: {},
-        series: [{
-            name: '男',
-            type: 'bar',
-            data: data1,
-            animationDelay: function (idx) {
-                return idx * 10;
-            }
-        }, {
-            name: '女',
-            type: 'bar',
-            data: data2,
-            animationDelay: function (idx) {
-                return idx * 10 + 100;
-            }
-        }],
-        animationEasing: 'elasticOut',
-        animationDelayUpdate: function (idx) {
-            return idx * 5;
         }
-    };
-    if (option && typeof option === "object") {
-        myChart.setOption(option, true);
-    }
-    resize(myChart);
+    });
 }
 
 /**
