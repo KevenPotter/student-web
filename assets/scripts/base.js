5b7ead20c59679f612ef99e6164c8d652b34d3e7
KevenPotter <myloveyim@qq.com>
2021-01-14 16:58:43 +0800

    Commit

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -7,26 +7,26 @@ $(document).ready(function () {
 });
 
 /*请求地址*/
-var studentManagementSystem = "http://localhost:8081";
-var studentManagementSystemWebSocket = "ws://localhost:8081";
-var studentImagesSystem = "http://www.123.com/images";
+let studentManagementSystem = "http://localhost:8081";
+let studentManagementSystemWebSocket = "ws://localhost:8081";
+let studentImagesSystem = "http://www.123.com/images";
 /*layer提示icon*/
-var GREEN_CHECK_MARK = 1;
-var RED_ERROR_MARK = 2;
-var YELLOW_QUESTION_MARK = 3;
-var GRAY_LOCKING_MARK = 4;
-var RED_CRYING_MARK = 5;
-var GREEN_SMILE_MARK = 6;
+let GREEN_CHECK_MARK = 1;
+let RED_ERROR_MARK = 2;
+let YELLOW_QUESTION_MARK = 3;
+let GRAY_LOCKING_MARK = 4;
+let RED_CRYING_MARK = 5;
+let GREEN_SMILE_MARK = 6;
 /*网站浏览人数*/
-var WEBSITE_VISITORS = 0;
+let WEBSITE_VISITORS = 0;
 /*公用学生编号*/
-var STUDENT_ID;
+let STUDENT_ID;
 /*公用教师编号*/
-var TEACHER_ID;
+let TEACHER_ID;
 /*公用系别编号*/
-var DEPARTMENT_ID;
+let DEPARTMENT_ID;
 /*公用专业编号*/
-var MAJOR_ID;
+let MAJOR_ID;
 
 /**
  * @author KevenPotter
@@ -38,7 +38,7 @@ function onlineUserCounts() {
         layerMsg('该浏览器暂不支持WebSocket', GREEN_SMILE_MARK, 3000);
         return;
     }
-    var onlineUserCountSocket = new WebSocket(studentManagementSystemWebSocket + "/dashboard");
+    let onlineUserCountSocket = new WebSocket(studentManagementSystemWebSocket + "/dashboard");
     onlineUserCountSocket.onmessage = function (msg) {
         $('#visits').text(msg.data);
         WEBSITE_VISITORS = msg.data;
